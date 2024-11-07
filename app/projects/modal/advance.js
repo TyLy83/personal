7e33a7482ec1bcafb5871e24ba4f68d7ad43b732
Ty Ly <ty.ly81@gmial.com>
2024-11-08 10:04:55 +1300

    fixed ' problems

--- app/projects/modal/advance.js
+++ app/projects/modal/advance.js
@@ -25,7 +25,7 @@ export default function Advance() {
             <div className="p-4">
                 <h4 className="mb-4 capitalize">What is Lorem Ipsum?</h4>
                 <p className="mb-4">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    {`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`}
                 </p>
             </div>
         </Modal>
